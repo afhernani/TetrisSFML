6b5dba734d8ab7204da364fdabe78698bba8ac81
Hernani <afhernani@gmail.com>
2026-05-20 01:36:13 +0100

    main para evitar crash de OpenGL Zink

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "Game.h"
+#include <cstdlib>
 
 int main() {
-    setenv("LIBGL_ALWAYS_SOFTWARE", "1", 1); // ← Añade esta línea
+       // Solo necesario en Linux/WSL para evitar el crash de OpenGL Zink
+#if defined(__linux__) || defined(__APPLE__)
+    setenv("LIBGL_ALWAYS_SOFTWARE", "1", 1);
+#endif
+
     Game game;
     game.run();
     return 0;
